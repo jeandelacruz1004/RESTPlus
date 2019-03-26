9fede95755a0495c91ee08aee7db796a47865e4d
Jean Dela Cruz <delacruzjean058@gmail.com>
2019-03-26 22:45:19 +0800

    Configuration Settings

---
+++ app/main/config.py
@@ -0,0 +1,41 @@
+import os
+
+
+postgres_local_base = 'postgresql://postgres:password@localhost:5432/FlaskRestPlus'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'password')
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    # uncomment the line below to use postgres
+    SQLALCHEMY_DATABASE_URI ='postgresql://postgres:password@localhost:5433/RESTPlus'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/RESTPlus'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/RESTPlus'
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    # uncomment the line below to use postgres
+     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/RESTPlus'
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
+)
+
+key = Config.SECRET_KEY
